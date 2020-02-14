2269a079bdc55a76ea4d410eb23c49fd8492521b
Jean Devalance <=>
2020-02-14 16:38:30 +0100

    FilterSlider Component + updated UI

--- client/src/components/LandFilters/LandFilters.js
+++ client/src/components/LandFilters/LandFilters.js
@@ -1,61 +1,60 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import './LandFilters.css';
 import Accordion from "react-bootstrap/Accordion";
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
-import FormLabel from "react-bootstrap/FormLabel";
-import FormControl from "react-bootstrap/FormControl";
-import FormGroup from "react-bootstrap/FormGroup";
+import FilterSlider from "./FilterSlider";
 
 function LandFilters({index, land, getLand}) {
-    let slideTimer;
+    let timer;
+    const initiated = useRef(true);
+    const [minRelevance, setMinRelevance] = useState(land.minRelevance);
+    const [maxDepth, setMaxDepth] = useState(land.maxDepth);
 
-    const load = params => {
-        clearTimeout(slideTimer);
-        slideTimer = setTimeout(_ => {
-            getLand(land.id, params);
-        }, 400);
+    useEffect(() => {
+        if (initiated.current) {
+            initiated.current = false;
+            return;
+        }
+        load();
+    }, [minRelevance, maxDepth]);
+
+    const applyRelevance = (value) => {
+        delay(setMinRelevance, value);
     };
 
-    const setMinRelevance = event => {
-        load({
-            minRelevance: event.target.value,
-            minDepth: document.getElementById("formDepth" + land.id).value
-        });
+    const applyDepth = (value) => {
+        delay(setMaxDepth, value);
     };
 
-    const setMinDepth = event => {
-        load({
-            minRelevance: document.getElementById("formRelevance" + land.id).value,
-            minDepth: event.target.value
-        })
+    const delay = (callback, value) => {
+        clearTimeout(timer);
+        timer = setTimeout((value) => {
+            callback(value);
+        }, 400, value);
+    }
+
+    const load = () => {
+        getLand(land.id, {minRelevance: minRelevance, maxDepth: maxDepth})
     };
 
     return (
         <Accordion.Collapse eventKey={index}>
             <Card.Body>
                 <Form>
-                    <FormGroup controlId={"formRelevance" + land.id}>
-                        <FormLabel>Minimum relevance</FormLabel>
-                        <FormControl type="range" min={land.minRelevance} max={land.maxRelevance} list={"rel_" + land.id}
-                                     className="form-control-range" onChange={setMinRelevance}/>
-                        <datalist id={"rel_" + land.id}>
-                            <option value={land.minRelevance} label={land.minRelevance}/>
-                            <option value={land.maxRelevance} label={land.maxRelevance}/>
-                        </datalist>
-                    </FormGroup>
-                    <FormGroup controlId={"formDepth" + land.id}>
-                        <FormLabel>Maximum depth</FormLabel>
-                        <FormControl type="range" min={land.minDepth} max={land.maxDepth} list={"dep_" + land.id} onChange={setMinDepth}/>
-                        <datalist id={"dep_" + land.id}>
-                            <option value="0" label="0"/>
-                            <option value="1"/>
-                            <option value="2"/>
-                            <option value="3" label="3"/>
-                        </datalist>
-                    </FormGroup>
+                    <FilterSlider label="Minimum relevance"
+                                  min={land.minRelevance}
+                                  max={land.maxRelevance}
+                                  defaultValue={land.minRelevance}
+                                  apply={applyRelevance}/>
+
+                    <FilterSlider label="Maximum depth"
+                                  min={land.minDepth}
+                                  max={land.maxDepth}
+                                  defaultValue={land.maxDepth}
+                                  apply={applyDepth}/>
                 </Form>
-                <p>{land.description}</p>
+                <p className="text-muted">{land.description}</p>
             </Card.Body>
         </Accordion.Collapse>
     );
